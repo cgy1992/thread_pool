261070451b6ae04d2935159d7b5f61dbfd71c9d7
wx7217242 <wx7217242@sina.com>
2016-12-27 21:27:23 +0800

    添加ThreadManager类

--- thread_manager.cc
+++ thread_manager.cc
@@ -1,1 +1,59 @@
 #include "thread_manager.h"
+#include "work_thread.h"
+#include "job_manager.h"
+#include <assert.h>
+
+ThreadManager::ThreadManager(size_t size) :
+    running_(false),
+    job_manager_(NULL),
+    thread_pool_(NULL)
+{
+    job_manager_ = new JobManager();
+    assert(job_manager_ != NULL);
+    
+    thread_pool_ = new ThreadPool(size);
+    assert(thread_pool_ != NULL);
+}
+
+ThreadManager::~ThreadManager()
+{
+    if (job_manager_ != NULL)
+        delete job_manager_;
+    
+    if (thread_pool_ != NULL)
+        delete thread_pool_;
+}
+
+void ThreadManager::Run()
+{
+    running_ = true;
+    
+    while (running_)
+    {
+        Job* job = job_manager_->GetJob();
+        if (job == NULL)
+        {
+            Thread::Sleep(1000);
+            continue;
+        }
+        
+        // TODO 根据相应的算法，决定什么时候扩容
+        Thread* thread = thread_pool_->GetIdleThread();
+        if (thread == NULL)
+        {
+            job_manager_->AddJob(job);
+            Thread::Sleep(1000);
+            continue;
+        }
+        
+        WorkThread* work_thread = static_cast<WorkThread*>(thread);
+        work_thread->set_job(job);
+        work_thread->set_thread_state(TS_BUSY);
+        work_thread->Resume();
+    }
+}
+
+
+
+
+
--- thread_manager.h
+++ thread_manager.h
@@ -1,33 +1,21 @@
 #ifndef THREAD_MANAGER_H_
 #define THREAD_MANAGER_H_
 
-#include "thread.h"
-#include "mutex.h"
-#include "condition.h"
-
 #include "thread_pool.h"
-
 #include <list>
-#include <stdint.h>
 
-class Job;
+const size_t DEFAULT_POOL_SIZE = 2;
+
+class JobManager;
 class ThreadPool;
 
 class ThreadManager
 {
 public:
-    typedef std::list<Job*> JobList;
-    
-    ThreadManager(size_t size = 10);
+    explicit ThreadManager(size_t size = DEFAULT_POOL_SIZE);
     ~ThreadManager();
 
     void Run();
-
-    void AddJob(Job* job);
-    void RemoveJob(Job* job);
-    Job* GetJob();
-    
-//    ThreadData& GetJobThreadData() { return mJobThreadData; }
 
     ThreadPool*  thread_pool() { return thread_pool_; }
     void IncreasePool(size_t size) { thread_pool_->Increase(size); }
@@ -35,19 +23,11 @@ public:
     void SetPoolSize(size_t size) { thread_pool_->set_pool_size(size); }
     void CleanUpPool() { thread_pool_->CleanUpPool(); }
 
-//    HANDLE GetExitEvent() const { return mExitEvent; }
-//    void SetRunEvent(ThreadData& threadData);
-
-//    void EnumProcess(void);
-
 private:
-//    static UINT __stdcall JobThreadProc(LPVOID pParam);
-
-private:
-    JobList job_list_;
+    bool running_;
+    JobManager* job_manager_;
     ThreadPool* thread_pool_;
-//    ThreadData mJobThreadData;
-//    HANDLE mExitEvent;
+    
 };
 
 #endif
